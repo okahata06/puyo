88504e6fa29a9a3374a7565a55b2bd66de5e85c9
DESKTOP-U0TU0SG\GA2A <aa>
2024-04-25 16:31:32 +0900

    start

---
+++ SusiBar/Assets/hiramatutest/タイトル/Spaceship.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    public RectTransform ship;
+    private int counter = 0;
+    private float move = 0.1f;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        ship.position += new Vector3(0, move, 0);
+        counter++;
+        if(counter==100)
+        {
+            counter = 0;
+            move *= -1;
+        }
+        
+    }
+}
