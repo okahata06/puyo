249306afc6fb9feb620cc7b3c1cc54c1e56536eb
DESKTOP-CMFB67U\GA2A <aa>
2024-05-13 12:27:40 +0900

    ドラッグできる。タグの配列取得やる

--- SusiBar/Assets/okahataairu/script/create.cs
+++ SusiBar/Assets/okahataairu/script/create.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class create : MonoBehaviour
+public class create : EventTrigger
 {
+
+    [SerializeField, Header("ネタの数")] int neta_kazu;
+
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
-        foreach (GameObject obj in objs)
-        {
-            Debug.Log(obj.gameObject.name);
+        //インスペクタで決めたネタ数の配列
+        int[] neta = new int[neta_kazu];
+        //配列に各ネタのタグを格納
+        for (int i=0; i<neta.Length; i++) 
+        { 
+
         }
+        //動作する
+        //GameObject[] objs = GameObject.FindGameObjectsWithTag("Player");
+        //foreach (GameObject obj in objs)
+        //{
+        //    Debug.Log(obj.gameObject.name);
+        //}
     }
 
     // Update is called once per frame
@@ -19,12 +35,11 @@ public class create : MonoBehaviour
     {
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
 
-            Debug.Log("asdf");
-        
+    //オブジェの上でドロップ
+    public override void OnDrop(PointerEventData data)
+    {
+        Debug.Log("OnDrop called.");
 
     }
-
 }
--- SusiBar/Assets/okahataairu/script/neta_Drag.cs
+++ SusiBar/Assets/okahataairu/script/neta_Drag.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class neta_Drag : MonoBehaviour
 {
+    [SerializeField,Header("ドラッグ中の座標")] int z_pos;
+
 
     bool cllick_down;
 
@@ -26,7 +28,7 @@ public class neta_Drag : MonoBehaviour
     private void OnMouseDrag()
     {
         //マウスの座標に移動
-        neta_p = Input.mousePosition;
+        neta_p =new Vector3(Input.mousePosition.x, Input.mousePosition.y, z_pos);
         this.gameObject.transform.position = neta_p;
 
 
