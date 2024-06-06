80b6163a4b7ba9ceb52b9aaad7f52bec05d10b20
MSI\GA2A <ads>
2024-06-06 12:21:41 +0900

    fa

--- SusiBar/Assets/okahataairu/script/create.cs
+++ SusiBar/Assets/okahataairu/script/create.cs
@@ -6,65 +6,70 @@ using UnityEngine.EventSystems;
 
 public class create : MonoBehaviour
 {
-
-    int neta_kazu = 3;
-
+    static int neta_kazu = 3;
+    GameObject[] neta =new GameObject[neta_kazu];
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+
+
+
         //インスペクタで決めたネタ数の配列
         //int[] neta = new int[neta_kazu];
 
 
         //配列に各ネタのタグを格納
-        for (int i = 0; i < neta_kazu; i++)
-        {
-            switch (i)
-            {
-                case 0:
-                    GameObject[] neta1 = GameObject.FindGameObjectsWithTag("neta1");
-                    foreach (GameObject obj in neta1)
-                    {
-                        Debug.Log(obj.gameObject.name);
-                    }
+        //for (int i = 0; i < neta_kazu; i++)
+        //{
+        //    switch (i)
+        //    {
+        //        case 0:
+        //            GameObject[] neta1 = GameObject.FindGameObjectsWithTag("neta1");
+        //            foreach (GameObject obj in neta1)
+        //            {
+        //                Debug.Log(obj.gameObject.name);
+        //            }
 
-                    break;
-                case 1:
-                    GameObject[] neta2 = GameObject.FindGameObjectsWithTag("neta2");
-                    foreach (GameObject obj in neta2)
-                    {
-                        Debug.Log(obj.gameObject.name);
-                    }
+        //            break;
+        //        case 1:
+        //            GameObject[] neta2 = GameObject.FindGameObjectsWithTag("neta2");
+        //            foreach (GameObject obj in neta2)
+        //            {
+        //                Debug.Log(obj.gameObject.name);
+        //            }
 
-                    break;
-                case 2:
-                    GameObject[] neta3 = GameObject.FindGameObjectsWithTag("neta3");
-                    foreach (GameObject obj in neta3)
-                    {
-                        Debug.Log(obj.gameObject.name);
-                    }
+        //            break;
+        //        case 2:
+        //            GameObject[] neta3 = GameObject.FindGameObjectsWithTag("neta3");
+        //            foreach (GameObject obj in neta3)
+        //            {
+        //                Debug.Log(obj.gameObject.name);
+        //            }
 
-                    break;
-                case 3:
-                    GameObject[] neta4 = GameObject.FindGameObjectsWithTag("neta4");
-                    foreach (GameObject obj in neta4)
-                    {
-                        Debug.Log(obj.gameObject.name);
-                    }
+        //            break;
+        //        case 3:
+        //            GameObject[] neta4 = GameObject.FindGameObjectsWithTag("neta4");
+        //            foreach (GameObject obj in neta4)
+        //            {
+        //                Debug.Log(obj.gameObject.name);
+        //            }
 
-                    break;
+        //            break;
 
-                default:
-                    break;
-            }
+        //        default:
+        //            break;
+        //    }
 
 
 
-        }
+        //}
         //動作する
+
+
+
     }
     
     // Update is called once per frame
@@ -72,10 +77,26 @@ public class create : MonoBehaviour
     {
 
     }
+    //触れると物理演算なしで判定獲得
     public void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.CompareTag("aa"))
-        Debug.Log("trigger.hit");
+        
+        if(col.gameObject.CompareTag("neta1"))
+        {
+            Debug.Log("neta1_hit");
+            
+        }
+        else if (col.gameObject.CompareTag("neta2"))
+        {
+            Debug.Log("neta2_hit");
+
+        }
+        else if (col.gameObject.CompareTag("neta3"))
+        {
+            Debug.Log("neta3_hit");
+            col.gameObject.transform.position = this.gameObject.transform.position;
+        }
+
 
     }
  
